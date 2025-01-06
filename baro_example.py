c355dbbb8c93449da7f66a93625fee876cc372a9
Nugine <nugine@foxmail.com>
2025-01-06 19:35:13 +0800

    example

---
+++ baro_example.py
@@ -0,0 +1,15 @@
+from RCAEval.e2e import baro
+from RCAEval.utility import download_data, read_data
+
+# download a sample data to data.csv
+download_data()
+
+# read data from data.csv
+data = read_data("data.csv")
+anomaly_detected_timestamp = 1692569339
+
+# perform root cause analysis
+root_causes = baro(data, anomaly_detected_timestamp)["ranks"]
+
+# print the top 5 root causes
+print("Top 5 root causes:", root_causes[:5])
