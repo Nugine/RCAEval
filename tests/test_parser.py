e4a6f0ecd2b512a1c09d307ebf4d4b3c6e4c75e6
Luan Pham <phamquiluan@gmail.com>
2025-04-10 15:53:15 +0000

    Update tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,12 +1,35 @@
+import pytest 
 from RCAEval.logparser import EventTemplate
 
 
 
-def test_load_template():
-    template = EventTemplate("received ad request (context_words=[<*>])")
-
-    assert not template.match("abc")
-    assert template.match("received ad request (context_words=[clothing])")
-    assert template.match("received ad request (context_words=[clothing, shoes])")
-
+@pytest.mark.parametrize("pattern, no_matches, matches", [
+    (
+        "received ad request (context_words=[<*>])",
+        ["abc", "received ad", "ad request (context_words=[])"],
+        [
+            "received ad request (context_words=[clothing])",
+            "received ad request (context_words=[clothing, shoes])",
+            "received ad request (context_words=[123])"
+        ]
+    ),
+    (
+        "SEVERE: Exception while executing runnable <*>",
+        ["SEVERE: Exception while executing runnable", "abc", ""],
+        [
+            "SEVERE: Exception while executing runnable io.grpc.internal.ServerImpl$JumpToApplicationThreadServerStreamListener$1HalfClosed@7d71091e",
+            "SEVERE: Exception while executing runnable io.grpc.internal.ServerImpl$JumpToApplicationThreadServerStreamListener$1HalfClosed@293e648c"
+        ]
+    ) 
+])
+def test_load_template(pattern, no_matches, matches):
+    template = EventTemplate(pattern)
+    
+    # Test strings that should NOT match
+    for no_match in no_matches:
+        assert not template.match(no_match)
+    
+    # Test strings that should match
+    for match in matches:
+        assert template.match(match)
 
