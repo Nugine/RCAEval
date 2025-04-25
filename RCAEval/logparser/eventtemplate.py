60ef074d73fdf456607c23790fbefc13c0e7d1bc
Luan Pham <phamquiluan@gmail.com>
2025-04-25 05:22:48 +0000

    Add event template class

---
+++ RCAEval/logparser/eventtemplate.py
@@ -0,0 +1,15 @@
+from .template import Template
+
+
+class EventTemplate(Template):
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+        
+    def is_match(self, event):
+        pass
+    
+    def load_templates(self, template_file):
+        pass
